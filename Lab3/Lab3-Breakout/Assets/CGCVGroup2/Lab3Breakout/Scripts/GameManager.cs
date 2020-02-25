271beb239f760c795e31bb6055713a82a7be92b3
beemc <58515034+beemc@users.noreply.github.com>
2020-02-25 22:27:21 +0100

    t2

--- Lab3/Lab3-Breakout/Assets/CGCVGroup2/Lab3Breakout/Scripts/GameManager.cs
+++ Lab3/Lab3-Breakout/Assets/CGCVGroup2/Lab3Breakout/Scripts/GameManager.cs
@@ -15,8 +15,14 @@ namespace CGCVGroup2.Lab3Breakout.Scripts
         [SerializeField] private Transform bricks;
         [SerializeField] private Toggle[] lives;
         [SerializeField] private RectTransform loseScreen, winScreen;
-        [SerializeField] private TextMeshProUGUI score;
+        [SerializeField] private TextMeshPro score;
         [SerializeField] private float time;
+
+        public GameObject Ball_Ob;
+        public GameObject Paddle_Ob;
+
+        public Rigidbody Ball_RB;
+        public Rigidbody Paddle_RB;
 
         public void CollideBrick(Collision other)
         {
@@ -33,18 +39,24 @@ namespace CGCVGroup2.Lab3Breakout.Scripts
         // Start is called before the first frame update
         void Start()
         {
+
             _BrickCount = 32;
             _LifeCount = 5;
-            loseScreen = null;
-            winScreen = null;
             _Score = 0;
             time = 5;
-            Time.timeScale = 0; 
+
+            loseScreen.gameObject.SetActive(false);
+            winScreen.gameObject.SetActive(false);
+            Ball_Ob = GameObject.Find("Ball");
+            Paddle_Ob = GameObject.Find("Paddle");
+            Ball_RB = Ball_Ob.GetComponent<Rigidbody>();
+            Paddle_RB = Paddle_Ob.GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
         private void Update()
         {
+
             if (_GameIsOver == true)
             {
 
@@ -59,27 +71,41 @@ namespace CGCVGroup2.Lab3Breakout.Scripts
 
                 if (Input.GetKey(KeyCode.Escape))
                 {
-                    print("ESCAPE");
-                    while ((Input.GetKey(KeyCode.Return)) == false)
-                    {
-                        print("x");
-                    }
-                    print("DE_ESCAPE");
+                    Time.timeScale = 0;
+                }
+
+                if (Input.GetKey(KeyCode.Return))
+                {
+                    Time.timeScale = 1;
+            
+                    print($"Ball {Ball_Ob}");
+                    print($"RB {Ball_RB}");
+   
                 }
             }
 
         }
 
-        void CollideWater()
+        public void CollideWater(Collision other)
         {
             if (_GameIsOver) return;
             _LifeCount--;
             //other.gameObject.SetActive(false);
             //score.text = $"Score: {++_Score}";
+            lives[_LifeCount].gameObject.SetActive(false);
+            Ball_RB.position = new Vector3(0, 2.5f, 0);
+            Paddle_RB.position = new Vector3(0, 0.75f, 0);
+
+            Ball_RB.velocity = new Vector3(UnityEngine.Random.Range(-10.0f,10.0f), 10.0f, 0);
+
+            
             if (_LifeCount > 0) return;
 
             _GameIsOver = true;
+            
+            //print($"LOSE : {loseScreen.gameObject} poes");
             loseScreen.gameObject.SetActive(true);
+            winScreen.gameObject.SetActive(false);
             Invoke(nameof(Restart), time);
         }
 
@@ -92,7 +118,7 @@ namespace CGCVGroup2.Lab3Breakout.Scripts
         {
             bricks = null;
             lives = null;
-            loseScreen = null;
+            //loseScreen = null;
             score = null;
             time = 5;
         }

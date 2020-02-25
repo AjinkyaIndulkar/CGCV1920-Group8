83112823d13529bf7090ffbadd3033d5a5080768
beemc <58515034+beemc@users.noreply.github.com>
2020-02-25 13:35:51 +0100

    Update GameManager.cs

--- Lab3/Lab3-Breakout/Assets/CGCVGroup2/Lab3Breakout/Scripts/GameManager.cs
+++ Lab3/Lab3-Breakout/Assets/CGCVGroup2/Lab3Breakout/Scripts/GameManager.cs
@@ -1,18 +1,79 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using System;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+namespace CGCVGroup2.Lab3Breakout.Scripts
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameManager : Singleton<GameManager>
     {
-        
+        private int _BrickCount, _LifeCount, _Score;
+        private bool _GameIsOver;
+        [SerializeField] private Transform bricks;
+        [SerializeField] private Toggle[] lives;
+        [SerializeField] private RectTransform loseScreen, winScreen;
+        [SerializeField] private TextMeshProUGUI score;
+        [SerializeField] private float time;
+
+        public void CollideBrick(Collision other)
+        {
+            if (_GameIsOver) return;
+            _BrickCount--;
+            other.gameObject.SetActive(false);
+            score.text = $"Score: {++_Score}";
+            if (_BrickCount > 0) return;
+
+            _GameIsOver = true;
+            winScreen.gameObject.SetActive(true);
+            Invoke(nameof(Restart), time);
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            _BrickCount = 32;
+            _LifeCount = 5;
+            loseScreen = null;
+            winScreen = null;
+            _Score = 0;
+            time = 5;
+            throw new DivideByZeroException();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        void CollideWater()
+        {
+            if (_GameIsOver) return;
+            _LifeCount--;
+            //other.gameObject.SetActive(false);
+            //score.text = $"Score: {++_Score}";
+            if (_LifeCount > 0) return;
+
+            _GameIsOver = true;
+            loseScreen.gameObject.SetActive(true);
+            Invoke(nameof(Restart), time);
+        }
+
+        void Restart()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        void Reset()
+        {
+            bricks = null;
+            lives = null;
+            loseScreen = null;
+            score = null;
+            time = 5;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
